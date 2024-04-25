525f5557b1fc8055c19507409cdac5f36a8ce151
KristyRojas <kristinarojas124@gmail.com>
2024-04-25 00:32:25 -0400

    intentando

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from './components/Home.jsx'
+import RickAndMortyApp from './RickAndMortyApp.jsx'
+import {createHashRouter, RouterProvider} from "react-router-dom"
 
+
+const router=createHashRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"/character",
+    element:<RickAndMorty/>
+  },
+  {
+    path:"/*",
+    element:<p>ERROR 404 PAGE NOT FOUND</p>
+  },
+]
+);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouteProvider router={router}/>
+    {/* <App /> */}
   </React.StrictMode>,
 )
